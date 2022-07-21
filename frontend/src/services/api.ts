3b2bd192f614597b7845714a060b7ee9cae641f9
Lucas Gomes <lucasglpes@gmail.com>
2022-07-20 22:59:06 -0300

    Update api

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,9 @@
 import axios from "axios";
-
+  
 const api = axios.create({
-    baseURL: 'http://localhost:5000',
+    baseURL: (process.env.NODE_ENV === 'development')
+    ? 'http://localhost:5000/'
+    : 'https://controle-de-estoque-app.herokuapp.com'
 });
 
 export default api;
